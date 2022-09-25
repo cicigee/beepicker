d6429e1a2173ff06ab0cf092ca455efcbd1c3541
Cecilia Gripenberg <22261830+cicigee@users.noreply.github.com>
2022-09-25 19:37:22 +0200

    Create build.sh

---
+++ macos/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+pip3 install pyinstaller
+python3 -m PyInstaller -w bee_picker.py  --osx-bundle-identifier org.cici.BeePicker --argv-emulation -n "Bee Picker"
+mv "dist/Bee Picker.app" /Applications/
