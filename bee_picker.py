39ed80c3392ac38ffdbd18cfd2906e34a26cb6b7
Cecilia Gripenberg <22261830+cicigee@users.noreply.github.com>
2022-09-25 16:24:39 +0200

    Update bee_picker.py
    Add support for 32-bit Windows

--- bee_picker.py
+++ bee_picker.py
@@ -12,7 +12,7 @@ configuration = {}
 if os.sys.platform == 'darwin' or os.sys.platform == 'linux':
     with open(os.environ['HOME']+'/.bee_picker.yaml') as file:
         configuration.update(yaml.load(file, Loader=yaml.FullLoader))
-if os.sys.platform == 'windows':
+if os.sys.platform == 'windows' or os.sys.platform == 'win32':
     with open(os.environ['USERPROFILE']+'/.bee_picker.yaml') as file:
         configuration.update(yaml.load(file, Loader=yaml.FullLoader))
 
